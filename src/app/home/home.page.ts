cd69fa918ff289f11eee70babc6afe3a96687e60
AdrianJavier7 <aperezmiranda@safareyes.es>
2025-01-18 19:41:30 +0100

    Menú inferior casi responsive (por alguna razon el del pc no se ve)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {NavbarInferiorComponent} from "../navbar-inferior/navbar-inferior.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, NavbarInferiorComponent],
+  standalone: true,
 })
 export class HomePage {
   constructor() {}
