671e972e997fe563db9f78ea046a7385c113cde7
jcanterla <jcanterlarufino@safareyes.es>
2025-01-21 13:39:11 +0100

    navbar superior terminado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {IonicModule} from "@ionic/angular";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, NavbarInferiorComponent],
+  imports: [IonicModule],
   standalone: true,
 })
 export class HomePage {
