b9475f668aa8f32eb63d30c8f38eaf0b3febf5a8
Jose Maria <jmateosmartin@safareyese.es>
2025-01-23 13:35:19 +0100

    Notificaciones

--- src/app/explorar/explorar.component.ts
+++ src/app/explorar/explorar.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {IonicModule} from "@ionic/angular";
 import {NavbarInferiorComponent} from "../navbar-inferior/navbar-inferior.component";
 import {NavbarSuperiorComponent} from "../navbar-superior/navbar-superior.component";
+import {addIcons} from "ionicons";
+import {notificationsOutline} from "ionicons/icons";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'app-explorar',
@@ -16,8 +19,13 @@ import {NavbarSuperiorComponent} from "../navbar-superior/navbar-superior.compon
 })
 export class ExplorarComponent  implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {
+    addIcons({"notifications-outline": notificationsOutline})
+  }
 
   ngOnInit() {}
 
+  navigateToNotificaciones() {
+    this.router.navigate(['/notificaciones']);
+  }
 }
